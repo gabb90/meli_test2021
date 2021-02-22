aa56494d74a1d96ae6dfe34bdb9569f14d761d72
ghocsman <ghocsman@leverbox.com>
2021-02-22 13:36:31 -0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\FuncionesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Bienvenido jefe de comunicaciones!';
+    return '!Bienvenido jefe de comunicaciones!';
 });
 
 Route::post('/topsecret_split/{id}', [FuncionesController::class, 'topsecret_split']);
