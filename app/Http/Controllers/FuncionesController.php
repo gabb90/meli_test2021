03a8164807153f48e7eb1421d7cb0116d998de87
ghocsman <ghocsman@leverbox.com>
2021-02-22 21:56:11 -0300

    fix

--- app/Http/Controllers/FuncionesController.php
+++ app/Http/Controllers/FuncionesController.php
@@ -46,6 +46,7 @@ class FuncionesController extends Controller
 
     private static function respuesta(array $kenobi, array $skywalker, array $sato)
     {
+        dd($sato);
         $position = Funcion::GetLocation($kenobi['distance'], $skywalker['distance'], $sato['distance']);
         $message = Funcion::GetMessage($kenobi['message'], $skywalker['message'], $sato['message']);
 
